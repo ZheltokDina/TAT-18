69419e18714cad0fda3f40fa55685b00348a450e
ZheltokDina <ZheltokDiana@yandex.by>
2018-03-01 23:36:50 +0300

    The draft version of the project with finding the same elements of the string

---
+++ FindMaxSubstring/EntryPoint.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace FindMaxSubstring
+{
+  /// <summary>
+  /// The entry point of the programm
+  /// </summary>
+  class EntryPoint
+  {
+    /// <summary>
+    /// For calling the main methods for check the user's string
+    /// </summary>
+    /// <param name="args">from command line</param>
+    static void Main(string[] args)
+    {
+      try
+      {
+        LengthChecker checker = new LengthChecker(args[0]);
+        checker.OutputResult(checker.FindMaxLengthOfSubstring());
+      }
+      catch(Exception ex)
+      {
+        Console.WriteLine(ex.Message);
+      }
+    }
+  }
+}
---
+++ FindMaxSubstring/LengthChecker.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+namespace FindMaxSubstring
+{
+  /// <summary>
+  /// This class finds maximum number of the same elements in the substring
+  /// </summary>
+  public class LengthChecker
+  {
+    private string userString;
+    public string UserString
+    {
+      get
+      {
+        return userString;
+      }
+      set
+      {
+        userString = value;
+        if (string.IsNullOrEmpty(userString))
+        {
+          throw new Exception("Your string is empty.");
+        }
+      }
+    }
+
+    /// <summary>
+    /// The trivial constructor with ToUpper() method 
+    /// </summary>
+    /// <param name="userChecker"></param>
+    public LengthChecker(string userString)
+    {
+      this.UserString = userString.ToUpper();
+    }
+
+    /// <summary>
+    /// This method is used for finding maximum length of the substring
+    /// </summary>
+    /// <returns></returns>
+    public int FindMaxLengthOfSubstring() 
+    {
+      int maxLengthCount = 0;
+      int currentCount = 0;
+      for (int i = 0; i < UserString.Length - 1; i++)
+      {
+        if (UserString[i].Equals(UserString[i + 1]))
+        {
+          currentCount++;                             
+        }
+        if (!UserString[i].Equals(UserString[i + 1]))
+        {
+          currentCount = 0;                         
+        }
+        else if(currentCount > maxLengthCount)        
+        {
+          maxLengthCount = currentCount;              
+        }
+      }
+      return maxLengthCount + 1;
+    }
+
+    /// <summary>
+    /// Is used for console output 
+    /// </summary>
+    /// <param name="resultLenght">which is displayed</param>
+    public void OutputResult(int resultLenght)
+    {
+      Console.WriteLine(resultLenght.ToString());
+    }
+  }
+}
