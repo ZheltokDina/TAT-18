7d448647a04a31b329dbee9bd6273efb9a0753e8
ZheltokDina <ZheltokDiana@yandex.by>
2018-03-04 12:52:42 +0300

    Task: finding the substring with even idices was added

---
+++ taskDEV2/EntryPoint.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace taskDEV2
+{
+  /// <summary>
+  /// The entry point of the programm
+  /// </summary>
+  class EntryPoint
+  {
+    static void Main(string[] args)
+    {
+      try
+      {
+        Console.WriteLine("Please, enter a string:");
+        string sequenceOfSymbols = Console.ReadLine();
+        StringCounter counter = new StringCounter(sequenceOfSymbols);
+        Console.WriteLine(counter.FindASubstringWithEvenIdices());
+      }
+      catch (Exception ex)
+      {
+        Console.WriteLine(ex.Message);
+      }
+    }
+  }
+}
---
+++ taskDEV2/StringCounter.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Text;
+
+namespace taskDEV2
+{
+  /// <summary>
+  /// This class finds substring with even idices
+  /// </summary>
+  class StringCounter
+  {
+    private string sequenceOfSymbols;
+    public string SequenceOfSymbols
+    {
+      get
+      {
+        return sequenceOfSymbols;
+      }
+      set
+      {
+        sequenceOfSymbols = value;
+        if (string.IsNullOrEmpty(sequenceOfSymbols))
+        {
+          throw new Exception("Your string is empty.");
+        }
+      }
+    }
+
+    /// <summary>
+    ///  The trivial constructor 
+    /// </summary>
+    /// <param name="sequenceOfSymbols"> string entered from console</param>
+    public StringCounter(string sequenceOfSymbols)
+    {
+      this.SequenceOfSymbols = sequenceOfSymbols;
+    }
+
+    /// <summary>
+    /// This method finds substring with even idices
+    /// </summary>
+    /// <returns>substring with even idices </returns>
+    public string FindASubstringWithEvenIdices()
+    {
+      StringBuilder line = new StringBuilder();
+      for (int indexOfSymbol = 0; indexOfSymbol < SequenceOfSymbols.Length; indexOfSymbol++)
+      {
+        if (indexOfSymbol % 2 == 0)
+        {
+          line.Append(SequenceOfSymbols[indexOfSymbol]);
+        }
+      }
+      return line.ToString();
+    }
+  }
+}
