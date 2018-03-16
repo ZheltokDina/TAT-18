b41ab166da6dad5c26a23abc2f68c4d7c836bf07
ZheltokDina <ZheltokDiana@yandex.by>
2018-03-16 14:56:20 +0300

    Added tests to taskDEV-2

---
+++ taskDEV2.tests/StringCreatorTests.cs
@@ -0,0 +1,29 @@
+using System;
+using Xunit;
+
+namespace taskDEV2.tests
+{
+  public class StringCreatorTests
+  {
+    [Fact]
+    public void ÑheckForTheFormationOfTheCorrectSubstring()
+    {
+      StringCreator creator = new StringCreator("abcdef");
+      Assert.Equal("ace", creator.GetASubstringWithEvenIndexes());
+    }
+    [Fact]
+    public void CheckForAStringThatContainsOneSymbol()
+    {
+      StringCreator creator = new StringCreator("a");
+      Assert.Equal("a", creator.GetASubstringWithEvenIndexes());
+    }
+    [Fact]
+    public void CheckTheCorrectnessThrownException()
+    {
+      Assert.Throws<ArgumentNullException>
+        (
+          () => new StringCreator(String.Empty)
+        );
+    }
+  }
+}
