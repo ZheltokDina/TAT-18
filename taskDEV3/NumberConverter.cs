8f3f0d69bcd551ffbb178348205542d3d9f55a76
ZheltokDina <ZheltokDiana@yandex.by>
2018-03-16 18:06:55 +0300

    Added tests to taskDEV3

--- taskDEV3/NumberConverter.cs
+++ taskDEV3/NumberConverter.cs
@@ -7,7 +7,7 @@ namespace taskDEV3
   /// <summary>
   /// This class is used for converting from decimal number system to another
   /// </summary>
-  class NumberConverter
+  public class NumberConverter
   {
     private const int NUMBER_TO_CHAR_START_POINT = 55;
     public BigInteger NumberInDecimalForm { get; private set; }
@@ -47,7 +47,7 @@ namespace taskDEV3
       }
       else
       {
-        do
+        while (bufferNumber != 0)
         {
           bufferNumber = NumberInDecimalForm / Radix;
           BigInteger remainder = NumberInDecimalForm % Radix;
@@ -60,7 +60,7 @@ namespace taskDEV3
             numberInNewRadix.Insert(0, (char)(NUMBER_TO_CHAR_START_POINT + remainder));
           }
           NumberInDecimalForm = bufferNumber;
-        } while (bufferNumber != 0);
+        } 
       }
       return numberInNewRadix.ToString();
     }
