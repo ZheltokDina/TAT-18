4697dc9fc846c8ec1ac270b933c90be94384bb67
ZheltokDina <ZheltokDiana@yandex.by>
2018-03-16 17:17:11 +0300

    Added tests to taskDEV-3

---
+++ taskDEV3.tests/NumberConverterExceptionTests.cs
@@ -0,0 +1,34 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace taskDEV3.tests
+{
+  [TestFixture]
+  public class NumberConverterExceptionTests
+  {
+    [Test]
+    public void CheckForExceptionWhenRadixLessThanAllowableValue()
+    {
+      Assert.Throws<ArgumentOutOfRangeException>
+       (
+          () => new NumberConverter(10, 1)
+       );
+    }
+    [Test]
+    public void CheckForExceptionWhenRadixMoreThanAllowableValue()
+    {
+      Assert.Throws<ArgumentOutOfRangeException>
+       (
+          () => new NumberConverter(55, 21)
+       );
+    }
+    [Test]
+    public void CheckForExceptionWhenNumberInDecimalFormLessThanAllowableValue()
+    {
+      Assert.Throws<ArgumentOutOfRangeException>
+       (
+          () => new NumberConverter(-1, 2)
+       );
+    }
+  }
+}
---
+++ taskDEV3.tests/NumberConverterTests.cs
@@ -0,0 +1,39 @@
+﻿using NUnit.Framework;
+
+namespace taskDEV3.tests
+{
+  [TestFixture]
+  public class NumberConverterTests
+  {
+    [Test]
+    public void CheckForTheConversionNumberWhenRadixIsMaxAllowableValue()
+    {
+      NumberConverter converter = new NumberConverter(10, 20);
+      Assert.AreEqual("A", converter.ConvertNumberToNewRadix());
+    }
+    [Test]
+    public void CheckForTheConversionNumberWhenRadixIsMinAllowableValue()
+    {
+      NumberConverter converter = new NumberConverter(10, 2);
+      Assert.AreEqual("1010",converter.ConvertNumberToNewRadix());
+    }
+    [Test]
+    public void CheckForTheCorrectnessOfTheConvertedNumberWithLetters()
+    {
+      NumberConverter converter = new NumberConverter(86478, 16);
+      Assert.AreEqual("151CE", converter.ConvertNumberToNewRadix());
+    }
+    [Test]
+    public void CheckForTheCorrectConversionWhenTheEnteredNumberIsOne()
+    {
+      NumberConverter converter = new NumberConverter(1, 2);
+      Assert.AreEqual("1", converter.ConvertNumberToNewRadix());
+    }
+    [Test]
+    public void CheckForTheCorrectConversionWhenTheEnteredNumberIsZero()
+    {
+      NumberConverter converter = new NumberConverter(0, 16);
+      Assert.AreEqual("0", converter.ConvertNumberToNewRadix());
+    }
+  }
+}
